4424dea916ec7d6e36c7e026b76ed5b942bb2ad0
jurgob <jurgob@users.noreply.github.com>
2025-08-15 14:45:41 +0100

    user endpoints

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,9 +5,34 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/health', (req, res) => {
+
+
+const statusRouter = express.Router();
+
+statusRouter.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() });
 });
+
+statusRouter.get('/status', (req, res) => {
+    res.status(200).json({ status: 'OK'});
+});
+
+
+const userRouter = express.Router();
+
+userRouter.get('/users', (req, res) => {
+    res.status(200).json({
+        users:[]
+    });
+});
+
+userRouter.post('/users', (req, res) => {
+    const { name , email} = req.body;
+    res.status(201).json({ name , email });
+});
+
+app.use('/api', statusRouter);
+app.use('/api', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- apps/backend/tests/server.test.ts
+++ apps/backend/tests/server.test.ts
@@ -2,7 +2,7 @@ import { expect, test, beforeAll, afterAll } from "vitest";
 import axios from "axios";
 
 const PORT = 3000;
-const BASE_URL = `http://localhost:${PORT}`;
+const BASE_URL = `http://localhost:${PORT}/api`;
 
 test('GET /health should return status OK', async () => {
   const response = await axios.get(`${BASE_URL}/health`);
@@ -11,4 +11,11 @@ test('GET /health should return status OK', async () => {
   expect(response.data).toHaveProperty('status', 'OK');
   expect(response.data).toHaveProperty('timestamp');
   expect(response.data.timestamp).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
+});
+
+test('GET /users should return status OK, empty list', async () => {
+  const response = await axios.get(`${BASE_URL}/users`);
+
+  expect(response.status).toBe(200);
+  expect(response.data).toEqual({users: []});
 });
